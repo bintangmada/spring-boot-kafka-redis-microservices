e974bdd432f8f8878d183b2e318aec1f1b11b159
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-04-30 11:20:16 +0700

    feat(order): add controller, service, and dto for placing orders

---
+++ order-service/src/main/java/com/example/order_service/dto/OrderRequest.java
@@ -0,0 +1,14 @@
+package com.example.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private String skuCode;
+    private Double price;
+    private Integer quantity;
+}
---
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.example.order_service.service;
+
+import com.example.order_service.dto.OrderRequest;
+import com.example.order_service.model.Order;
+import com.example.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setSkuCode(orderRequest.getSkuCode());
+        order.setPrice(orderRequest.getPrice());
+        order.setQuantity(orderRequest.getQuantity());
+
+        orderRepository.save(order);
+        // Nanti di sini kita akan tambahkan logika Kafka untuk memberitahu Inventory
+    }
+}
